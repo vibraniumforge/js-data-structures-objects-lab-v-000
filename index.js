71ef9447c52ec287bdd82b52af925c710ae948a9
Greg Morgan <gregm9999@gmail.com>
2019-01-09 00:38:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 
 function deleteFromDriverByKey(driver, key) {
   let newDriver={...driver}
-  console.log(driver, key, newDriver)
+  console.log(driver)
+  console.log(key)
+  console.log(newDriver)
   delete newDriver.key;
   return newDriver;
 }
